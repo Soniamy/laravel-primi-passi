f4806e1f6d7e01810ed15259beb4c42b016c815b
Soniamy <marianna.marzullo@gmail.com>
2024-02-22 15:28:30 +0100

    BONUS: creazione di altre pagine collegate tra loro attraverso le rotte con web.php

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Laravel</title>
+</head>
+<body>
+    <h1 class="text-center mb-4  ">Contact</h1>
+     <h5>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui earum quas veritatis necessitatibus hic minus. Quae quam aut ducimus debitis rerum inventore reiciendis ullam obcaecati tempora repellendus. Eius, dolore accusantium.</h5>
+    <div class="d-flex justify-content-center m-4  ">
+        <button type="button" class="btn btn-danger ">
+            <a href="/">Home</a>
+        </button>
+    </div>
+</body>
+</html>
